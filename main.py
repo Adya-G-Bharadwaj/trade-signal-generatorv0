76b6e3c9218e63c904bec594d56f9423f929a0ac
Adya Bharadwaj <adyagopibharadwaj@gmail.com>
2026-06-01 21:15:47 -0500

    Function details

---
+++ main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+#Loading pricing data
+
+#Create features: momentum, rolling volatility
+
+#Generate buy sell hold labels
+
+#Train model
+
+#Visualization
+
+#Main method: Load and process data, standardize features, train/test split, train model, predict on all data, plot
